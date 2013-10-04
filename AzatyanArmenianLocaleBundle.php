cefd224519ced635072ca12579d4c8414015eb5e
Tigran <tigran@azatyan.info>
2013-10-04 21:00:41 +0400

    dzvadzex

--- AzatyanArmenianLocaleBundle.php
+++ AzatyanArmenianLocaleBundle.php
@@ -4,6 +4,10 @@ namespace Azatyan\ArmenianLocaleBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class AzatyanArmenianLocaleBundle
+ * @package Azatyan\ArmenianLocaleBundle
+ */
 class AzatyanArmenianLocaleBundle extends Bundle
 {
 }
