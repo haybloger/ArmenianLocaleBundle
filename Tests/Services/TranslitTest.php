032403ad15c5fec9ab17481a70be1bb6abd68f11
Tigran Azatyan <tigran.azatyan@gmail.com>
2013-10-04 19:00:49 +0400

    Test Sceleton

--- Tests/Services/TranslitTest.php
+++ Tests/Services/TranslitTest.php
@@ -1,2 +1,26 @@
 <?php
+
+
+/** 
+ * 
+ * Test Translit Class
+ * 
+ */
+
+
+
+
 namespace Azatyan\ArmenianLocaleBundle\Tests\Services;
+
+
+class TrasnlitTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLatinToArmenian()
+    {
+
+        $input = "Barev";
+        $output = "Բարև";
+        
+        $this->assertEquals($input, $output);
+    }
+}
